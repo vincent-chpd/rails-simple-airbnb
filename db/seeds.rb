ca2941c9ed376a4c153e96321b423df8ba443a7e
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2024-02-08 21:32:23 +0000

    create model, controller and view for flat

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,31 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Pituresque Kensington Studio',
+  address: 'Kensington London',
+  description: 'Vicarage Gate is just a 3 minute walk from Notting Hill Gate tube station and a very short walking distance of Kensington High Street, Kensington Palace, Portobello Road Market, and the stunning Holland Park. It is a beautiful location and very tranquil.',
+  price_per_night: 81,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Entire rental unit in Greater London',
+  address: 'Canary Wharf London',
+  description: 'Super stylish apartment with fantastic views over the Thames and central London',
+  price_per_night: 170,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Self-contained apartment London',
+  address: 'Shepherd\'Bush London W9 1DT',
+  description: 'Newly modernised, with a king size bed, a shower & bath, an open plan fully fitted kitchen/ living room, separate laundry area. Direct access to terrace which is south facing',
+  price_per_night: 95,
+  number_of_guests: 2
+)
